0e3703fd26a16714bf1f6bf5f64697f023c51ac8
Joao Paulo <papaulo1010202008@gmail.com>
2021-02-15 16:26:44 -0300

    Remove unnecessary white spaces.

--- libdiscord.h
+++ libdiscord.h
@@ -785,21 +785,21 @@ enum {
 https://discord.com/developers/docs/resources/guild#guild-object-guild-features */
 namespace guild_features {
 enum {
-  INVITE_SPLASH,                      // guild has access to set an invite splash background
-  VIP_REGIONS,                        // guild has access to set 384kbps bitrate in voice (previously VIP voice servers)
-  VANITY_URL,                         // guild has access to set a vanity URL
-  VERIFIED,                           // guild is verified
-  PARTNERED,                          // guild is partnered
-  COMMUNITY,                          // guild can enable welcome screen, Membership Screening, and discovery, and receives community updates
-  COMMERCE,                           // guild has access to use commerce features (i.e. create store channels)
-  NEWS,                               // guild has access to create news channels
-  DISCOVERABLE,                       // guild is able to be discovered in the directory
-  FEATURABLE,                         // guild is able to be featured in the directory
-  ANIMATED_ICON,                      // guild has access to set an animated guild icon
-  BANNER,                             // guild has access to set a guild banner image
-  WELCOME_SCREEN_ENABLED,             // guild has enabled the welcome screen
-  MEMBER_VERIFICATION_GATE_ENABLED,   // guild has enabled Membership Screening
-  PREVIEW_ENABLED                     // guild can be previewed before joining via Membership Screening or the directory
+  INVITE_SPLASH,                    // guild has access to set an invite splash background
+  VIP_REGIONS,                      // guild has access to set 384kbps bitrate in voice (previously VIP voice servers)
+  VANITY_URL,                       // guild has access to set a vanity URL
+  VERIFIED,                         // guild is verified
+  PARTNERED,                        // guild is partnered
+  COMMUNITY,                        // guild can enable welcome screen, Membership Screening, and discovery, and receives community updates
+  COMMERCE,                         // guild has access to use commerce features (i.e. create store channels)
+  NEWS,                             // guild has access to create news channels
+  DISCOVERABLE,                     // guild is able to be discovered in the directory
+  FEATURABLE,                       // guild is able to be featured in the directory
+  ANIMATED_ICON,                    // guild has access to set an animated guild icon
+  BANNER,                           // guild has access to set a guild banner image
+  WELCOME_SCREEN_ENABLED,           // guild has enabled the welcome screen
+  MEMBER_VERIFICATION_GATE_ENABLED, // guild has enabled Membership Screening
+  PREVIEW_ENABLED                   // guild can be previewed before joining via Membership Screening or the directory
 };
 } // namespace guild_features
 

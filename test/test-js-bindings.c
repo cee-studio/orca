fd23e03d71974d05d913295d37a9de3d85e4eaf2
LucasMull <muller.lucas@Hotmail.com>
2021-07-26 14:57:17 -0300

    feat(test-js-bindings): add test for Statement.prototype.get()

--- test/test-js-bindings.c
+++ test/test-js-bindings.c
@@ -10,10 +10,11 @@
 const char *handle=NULL; /* handle to stowed away js function */
 const char *g_config_file;
 
-#define DB_NAME  "\"test-js-bindings.db\""
-#define SQL_EXEC_STMT "\"DROP TABLE IF EXISTS cats;"              \
-                      "CREATE TABLE cats (name TEXT, age INT);\""
-#define SQL_PREPARE_STMT "\"INSERT INTO cats (name, age) VALUES (?, ?)\""
+#define DB_NAME  "'test-js-bindings.db'"
+#define SQL_EXEC_STMT "'DROP TABLE IF EXISTS cats;"              \
+                      "CREATE TABLE cats (name TEXT, age INT);'"
+#define SQL_PREPARE_INSERT_STMT "'INSERT INTO cats (name, age) VALUES (?, ?)'"
+#define SQL_PREPARE_SELECT_STMT "'SELECT age FROM cats WHERE name = ?'"
 
 void js_request(js_State *J)
 {
@@ -35,14 +36,18 @@ int main(void)
   log_set_quiet(true);
 
   js_State *J = js_newstate(NULL, NULL, JS_STRICT);
+  jssqlite3_init(J);
+  jsua_init(J);
 
   /* TEST SQLITE3 BINDING */
-  jssqlite3_init(J);
   js_dostring(J, "var db = new Database();");
   js_dostring(J, "db.open("DB_NAME");");
   js_dostring(J, "db.exec("SQL_EXEC_STMT");");
-  js_dostring(J, "var stmt = db.prepare("SQL_PREPARE_STMT");");
+  js_dostring(J, "var stmt = db.prepare("SQL_PREPARE_INSERT_STMT");");
   js_dostring(J, "stmt.run('Joey', 2);");
+  js_dostring(J, "stmt = db.prepare("SQL_PREPARE_SELECT_STMT");");
+  js_dostring(J, "var cat = stmt.get('Joey');");
+  js_dostring(J, "print(JSON.stringify(cat));");
   js_dostring(J, "db.close();");
 
   /* TEST USER-AGENT BINDING */

2b43ce582865ce722312c6d76d95e44618e73d01
LucasMull <muller.lucas@Hotmail.com>
2021-07-25 22:36:35 -0300

    fix(js_sqlite3): Statement.prototype.run() wrapping execution flow * feat(js_sqlite3): improve error checking

--- common/js_sqlite3.c
+++ common/js_sqlite3.c
@@ -11,6 +11,8 @@
 
 #include "js_sqlite3.h"
 
+#define IS_SKIPPED(c) ((c)==' ' || (c)==';' || ((c)>='\t'&&(c)<='\r'))
+
 struct stmt_cxt {
   sqlite3 *db;
   sqlite3_stmt *stmt;
@@ -37,7 +39,7 @@ static void
 Database_prototype_open(js_State *J)
 {
   if (!js_isstring(J, 1)) { 
-    js_error(J, "Expected 'first' argument to be 'a string'"); 
+    js_typeerror(J, "Expected 'first' argument to be 'a string'"); 
   }
 
   sqlite3 **db = js_touserdata(J, 0, "Database");
@@ -61,14 +63,30 @@ static void
 Database_prototype_exec(js_State *J)
 {
   if (!js_isstring(J, 1)) { 
-    js_error(J, "Expected 'first' argument to be 'a string'"); 
+    js_typeerror(J, "Expected 'first' argument to be 'a string'"); 
   }
 
   sqlite3 **db = js_touserdata(J, 0, "Database");
-  const char *sql = js_tostring(J, 1);
-  if (SQLITE_OK != sqlite3_exec(*db, sql, 0, 0, NULL)) {
-    js_error(J, "Couldn't run SQL statements: %s", sqlite3_errmsg(*db)); 
+  sqlite3_stmt *stmt;
+  const char *sql = js_tostring(J, 1), *tail;
+
+  while (1) {
+    while (IS_SKIPPED(*sql)) ++sql;
+
+    if (SQLITE_OK != sqlite3_prepare_v2(*db, sql, -1, &stmt, &tail)) {
+      sqlite3_finalize(stmt);
+      js_error(J, "Failed to execute statement: %s", sqlite3_errmsg(*db));
+    }
+
+    sql = tail;
+    if (!stmt) break;
+
+    while (SQLITE_ROW == sqlite3_step(stmt))
+      continue;
+    if (SQLITE_OK != sqlite3_finalize(stmt))
+      break;
   }
+
   js_pushundefined(J);
 }
 
@@ -76,7 +94,7 @@ static void
 Database_prototype_prepare(js_State *J)
 {
   if (!js_isstring(J, 1)) { 
-    js_error(J, "Expected 'first' argument to be 'a string'"); 
+    js_typeerror(J, "Expected 'first' argument to be 'a string'"); 
   }
 
   sqlite3 **db = js_touserdata(J, 0, "Database");
@@ -86,11 +104,11 @@ Database_prototype_prepare(js_State *J)
   js_getglobal(J, "Statement");
   js_copy(J, 0); // push 'this'
   if (js_pconstruct(J, 1)) { // push Statement to top of stack
-    js_error(J, "Failed to call 'new Statement'");
+    js_referenceerror(J, "Failed to call 'new Statement(this)'");
   }
 
-  struct stmt_cxt *stmt_cxt = js_touserdata(J, -1, "Statement");
-  if (SQLITE_OK != sqlite3_prepare(*db, sql, -1, &stmt_cxt->stmt, NULL)) {
+  struct stmt_cxt *cxt = js_touserdata(J, -1, "Statement");
+  if (SQLITE_OK != sqlite3_prepare_v2(*db, sql, -1, &cxt->stmt, NULL)) {
     js_error(J, "Failed to execute statement: %s", sqlite3_errmsg(*db));
   }
 }
@@ -125,20 +143,20 @@ jssqlite3_db_init(js_State *J)
 }
 
 static void
-destroy_Statement(js_State *J, void *p_stmt_cxt) {
-  if (p_stmt_cxt) free(p_stmt_cxt);
+destroy_Statement(js_State *J, void *p_cxt) {
+  if (p_cxt) free(p_cxt);
 }
 
 static void 
 new_Statement(js_State *J)
 {
-  struct stmt_cxt *stmt_cxt = malloc(sizeof *stmt_cxt);
+  struct stmt_cxt *cxt = malloc(sizeof *cxt);
   sqlite3 **p_db = js_touserdata(J, 1, "Database");
-  stmt_cxt->db = *p_db;
+  cxt->db = *p_db;
 
   js_currentfunction(J);
   js_getproperty(J, -1, "prototype");
-  js_newuserdata(J, "Statement", stmt_cxt, &destroy_Statement);
+  js_newuserdata(J, "Statement", cxt, &destroy_Statement);
 }
 
 static int
@@ -146,7 +164,7 @@ jssqlite3_bind(js_State *J, int idx, sqlite3_stmt *stmt)
 {
   switch (js_type(J, idx)) {
   case JS_ISSTRING:
-      return sqlite3_bind_text(stmt, idx, js_tostring(J, idx), -1, SQLITE_TRANSIENT);
+      return sqlite3_bind_text(stmt, idx, js_tostring(J, idx), -1, SQLITE_STATIC);
   case JS_ISUNDEFINED:
   case JS_ISNULL:
       return sqlite3_bind_null(stmt, idx);
@@ -155,7 +173,7 @@ jssqlite3_bind(js_State *J, int idx, sqlite3_stmt *stmt)
   case JS_ISNUMBER:
       return sqlite3_bind_double(stmt, idx, js_tonumber(J, idx));
   default:
-      js_error(J, "Can't bind value of type '%s'", js_typeof(J, idx));
+      js_referenceerror(J, "Can't bind value of type '%s'", js_typeof(J, idx));
       break;
   }
   return -1;
@@ -164,36 +182,55 @@ jssqlite3_bind(js_State *J, int idx, sqlite3_stmt *stmt)
 static void 
 Statement_prototype_run(js_State *J)
 {
-  struct stmt_cxt *stmt_cxt = js_touserdata(J, 0, "Statement");
-  int ret_code;
-
-  int nparam = js_gettop(J);
-  for (int i=1; i < nparam; ++i) {
-    ret_code = jssqlite3_bind(J, i, stmt_cxt->stmt);
-    if (SQLITE_OK != ret_code) {
-      sqlite3_finalize(stmt_cxt->stmt);
-      js_error(J, "Failed to bind parameter No#%d of type '%s': %s", i, js_typeof(J, i), sqlite3_errstr(ret_code));
-    }
-
-    while (1) {
-      ret_code = sqlite3_step(stmt_cxt->stmt);
-      if (SQLITE_ROW == ret_code)
-        continue;
-      else if (SQLITE_DONE == ret_code)
-        break;
-      else {
-        sqlite3_finalize(stmt_cxt->stmt);
-        js_error(J, "%s", sqlite3_errstr(ret_code));
-      }
-    }
-
-    sqlite3_reset(stmt_cxt->stmt);
-    sqlite3_clear_bindings(stmt_cxt->stmt);
+  if (!js_isstring(J, 1)) { 
+    js_typeerror(J, "Expected 'first' argument to be 'a string'"); 
   }
 
-  sqlite3_finalize(stmt_cxt->stmt);
-  // @todo return info object
-  js_pushundefined(J);
+  struct stmt_cxt *cxt = js_touserdata(J, 0, "Statement");
+  int nparam = js_gettop(J), 
+      expect_nparam = sqlite3_bind_parameter_count(cxt->stmt);
+  int status;
+  int nrow=0;
+
+  if (js_try(J)) {
+    fprintf(stderr, "%s\n", js_tostring(J, -1));
+    sqlite3_reset(cxt->stmt);
+    sqlite3_clear_bindings(cxt->stmt);
+
+    js_pop(J, 1); // error object
+    js_pushundefined(J);
+    return;
+  }
+
+  if (nparam-1 != expect_nparam) {
+    js_referenceerror(J, "Expect %d parameters, got %d instead",
+      expect_nparam, nparam-1);
+  }
+
+  for (int i=1; i < nparam; ++i) {
+    status = jssqlite3_bind(J, i, cxt->stmt);
+    if (SQLITE_OK != status) {
+      js_rangeerror(J, "Failed to bind parameter No#%d of type '%s': %s",
+          i, js_typeof(J, i), sqlite3_errstr(status));
+    }
+  }
+
+  while (SQLITE_ROW == (status = sqlite3_step(cxt->stmt))) {
+    ++nrow;
+  }
+  if (SQLITE_DONE != status) {
+    js_evalerror(J, "Failed to evaluate SQL statement: %s", sqlite3_errstr(status));
+  }
+  sqlite3_reset(cxt->stmt);
+  sqlite3_clear_bindings(cxt->stmt);
+
+  js_newobject(J); // return info object
+  {
+    js_pushnumber(J, (double)nrow);
+    js_setproperty(J, -2, "changes");
+  }
+
+  js_endtry(J);
 }
 
 static void 

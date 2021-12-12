7475497d6aaef5d44da9540355463cc6e1996a93
lcsmuller <muller.lucas@Hotmail.com>
2021-12-12 13:16:52 -0300

    chore(bot-echo.c): simplify

--- examples/bot-echo.c
+++ examples/bot-echo.c
@@ -13,14 +13,13 @@ void on_ready(struct discord *client, const struct discord_user *bot)
 
 void on_reaction_add(struct discord *client,
                      const struct discord_user *bot,
-                     const uint64_t user_id,
-                     const uint64_t channel_id,
-                     const uint64_t message_id,
-                     const uint64_t guild_id,
+                     const u64_snowflake_t user_id,
+                     const u64_snowflake_t channel_id,
+                     const u64_snowflake_t message_id,
+                     const u64_snowflake_t guild_id,
                      const struct discord_guild_member *member,
                      const struct discord_emoji *emoji)
 {
-  // make sure bot doesn't echoes other bots
   if (member->user->bot) return;
 
   discord_create_reaction(client, channel_id, message_id, emoji->id,
@@ -31,22 +30,20 @@ void on_message_create(struct discord *client,
                        const struct discord_user *bot,
                        const struct discord_message *msg)
 {
-  // make sure bot doesn't echoes other bots
   if (msg->author->bot) return;
 
-  struct discord_create_message_params params = { .content = msg->content };
+  struct discord_create_message_params
+    params = { .content = msg->content,
+               .message_reference =
+                 !msg->referenced_message
+                   ? NULL
+                   : &(struct discord_message_reference){
+                     .message_id = msg->referenced_message->id,
+                     .channel_id = msg->channel_id,
+                     .guild_id = msg->guild_id,
+                   } };
 
-  struct discord_message_reference msg_ref;
-  if (msg->referenced_message) {
-    msg_ref = (struct discord_message_reference){
-      .message_id = msg->referenced_message->id,
-      .channel_id = msg->channel_id,
-      .guild_id = msg->guild_id,
-    };
-    params.message_reference = &msg_ref;
-  }
-
-  discord_create_message(client, msg->channel_id, &params, NULL);
+  discord_create_message_async(client, msg->channel_id, &params, NULL);
 }
 
 void on_message_update(struct discord *client,
@@ -56,40 +53,35 @@ void on_message_update(struct discord *client,
   struct discord_create_message_params params = {
     .content = "I see what you did there."
   };
-  discord_create_message(client, msg->channel_id, &params, NULL);
+
+  discord_create_message_async(client, msg->channel_id, &params, NULL);
 }
 
 void on_message_delete(struct discord *client,
                        const struct discord_user *bot,
-                       const uint64_t id,
-                       const uint64_t channel_id,
-                       const uint64_t guild_id)
+                       const u64_snowflake_t id,
+                       const u64_snowflake_t channel_id,
+                       const u64_snowflake_t guild_id)
 {
   struct discord_create_message_params params = {
     .content = "Did that message just disappear?"
   };
-  discord_create_message(client, channel_id, &params, NULL);
+
+  discord_create_message_async(client, channel_id, &params, NULL);
 }
 
 void on_message_delete_bulk(struct discord *client,
                             const struct discord_user *bot,
-                            const ja_u64 **ids,
-                            const uint64_t channel_id,
-                            const uint64_t guild_id)
+                            const u64_snowflake_t **ids,
+                            const u64_snowflake_t channel_id,
+                            const u64_snowflake_t guild_id)
 {
   char text[128];
-  snprintf(text, sizeof(text), "Ouch! Where did those %zu messages go?",
-           ntl_length((ntl_t)ids));
-  struct discord_create_message_params params = { .content = text };
-  discord_create_message(client, channel_id, &params, NULL);
-}
+  sprintf(text, "Where did those %zu messages go?", ntl_length((ntl_t)ids));
 
-enum discord_event_scheduler scheduler(struct discord *client,
-                                       struct discord_user *bot,
-                                       struct sized_buffer *event_data,
-                                       enum discord_gateway_events event)
-{
-  return DISCORD_EVENT_WORKER_THREAD;
+  struct discord_create_message_params params = { .content = text };
+
+  discord_create_message(client, channel_id, &params, NULL);
 }
 
 int main(int argc, char *argv[])
@@ -104,9 +96,6 @@ int main(int argc, char *argv[])
 
   struct discord *client = discord_config_init(config_file);
   assert(NULL != client && "Couldn't initialize client");
-
-  /* trigger event callbacks in a multi-threaded fashion */
-  discord_set_event_scheduler(client, &scheduler);
 
   discord_set_on_ready(client, &on_ready);
   discord_set_on_message_create(client, &on_message_create);
